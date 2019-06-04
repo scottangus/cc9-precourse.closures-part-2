9ef1381c9c180e07c2661507ad2acfc908d124ac
Scott Angus <scottdangus@gmail.com>
2019-06-04 01:31:10 -0400

    Wrote starting guess and reset functions for gameGenerator.

--- src/closures.js
+++ src/closures.js
@@ -8,7 +8,15 @@ function randomInteger(n) {
   return Math.floor(Math.random() * (n + 1));
 }
 
-function gameGenerator() {}
+function gameGenerator(input) {
+  var gameInstance = { boundLimit : input,
+                      gameNumber : randomInteger(input),
+                      guesses : 0,
+                      guess(guessNum){return guessNum == this.gameNumber ? true : false;},
+                      reset(){this.gameNumber = randomInteger(this.boundLimit);this.guesses = 0;},
+  };
+  return gameInstance;
+}
 
 function accountGenerator(initial) {
   let balance = initial;
